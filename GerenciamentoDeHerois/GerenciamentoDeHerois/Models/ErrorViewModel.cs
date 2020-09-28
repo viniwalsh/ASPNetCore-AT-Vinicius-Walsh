11c15c71b4eda19ff0d8f9685109b92a0f89df4a
Walsh <vmw.walsh@gmail.com>
2020-09-28 05:27:52 -0300

    Change project - fix errors

---
+++ GerenciamentoDeHerois/GerenciamentoDeHerois/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GerenciamentoDeHerois.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
